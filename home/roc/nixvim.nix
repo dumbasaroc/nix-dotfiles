b1c5c5a6fdaaf43b14728e5b66c95794441f12cd
DumbAsARoc <timbishop514@gmail.com>
2026-04-30 22:26:55 -0400

    Thu Apr 30 10:26:55 PM EDT 2026

--- home/roc/nixvim.nix
+++ home/roc/nixvim.nix
@@ -6,27 +6,24 @@
   programs.nixvim = {
     enable = true;
 
-    plugins = {
+    # Language Servers
+    lsp = {
+      servers = {
+        clangd = {
+          enable = true;
+        };
 
-      # Language Servers
-      lsp = {
-        servers = {
-          clangd = {
-            enable = true;
-          };
+        lua_ls = {
+          enable = true;
+        };
 
-          lua_ls = {
-            enable = true;
-          };
-
-          rust_analyzer = {
-            enable = true;
-            installRustc = true;
-            installCargo = true;
-          };
+        rust_analyzer = {
+          enable = true;
         };
       };
-      
+    };
+
+    plugins = {
       # LSP Config
       lspconfig = {
         enable = true;
