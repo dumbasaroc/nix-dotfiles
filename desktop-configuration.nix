8c2e2ce098e8d46cf9f9ebf977ef492e610f247c
DumbAsARoc <timbishop514@gmail.com>
2026-05-17 18:29:06 -0400

    Sun May 17 06:29:06 PM EDT 2026

--- desktop-configuration.nix
+++ desktop-configuration.nix
@@ -1,9 +1,20 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
+  environment.variables = {
+    RUSTICL_ENABLE = "radeonsi";
+  };
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
   hardware.graphics.extraPackages = with pkgs; [
     mesa.opencl
+  ];
+
+  nixpkgs.config.rocmSupport = true;
+
+  environment.systemPackages = with pkgs; [
+    clinfo
+    fastfetch
+    blender
   ];
 }
