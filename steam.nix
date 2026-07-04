d0bb32422743be3873972d9a2299ffb0c8dac902
DumbAsARoc <timbishop514@gmail.com>
2026-07-04 16:21:55 -0400

    Sat Jul  4 04:21:55 PM EDT 2026

--- steam.nix
+++ steam.nix
@@ -13,5 +13,18 @@
     # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     # localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+    gamescopeSession = {
+      enable = true;
+    };
+  };
+
+  programs.opengamepadui = {
+    enable = true;
+    inputplumber.enable = true;
+    powerstation.enable = true;
+    gamescopeSession = {
+      enable = true;
+    };
   };
 }
