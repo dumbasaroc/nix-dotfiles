f87d1f43f32794e4b228051b53f2094f23ed62d4
DumbAsARoc <timbishop514@gmail.com>
2026-07-14 13:01:34 -0400

    Tue Jul 14 01:01:34 PM EDT 2026

---
+++ sddm.nix
@@ -0,0 +1,16 @@
+
+{ config, lib, pkgs, inputs, ... }: let
+  sddm-astronaut = (pkgs.sddm-astronaut.override {
+    embeddedTheme = "astronaut";
+  });
+in {
+  environment.systemPackages = [ sddm-astronaut ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    extraPackages = with pkgs; [
+      kdePackages.qtmultimedia # Apparently required for video/audio backgrounds
+    ];
+    theme = "sddm-astronaut-theme";
+  };
+}
